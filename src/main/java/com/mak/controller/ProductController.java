785c8c69a6920edfdfd0455f2fc34ffb70f08a9b
Md Maksud <mdmaksud568@gmail.com>
2026-01-04 23:27:57 +0530

    Update ProductController.java

--- src/main/java/com/mak/controller/ProductController.java
+++ src/main/java/com/mak/controller/ProductController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mak.Entity.Product;
 import com.mak.service.ProductService;
-
-@Controller
+@RestController
 public class ProductController {
 	
 	@Autowired
